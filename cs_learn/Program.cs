05839a58d5f86721fe0e57997b8c7b7a9e9e6ea7
wangzhiyu@maxwell-gp.com.cn <WangZhiyu>
2023-07-25 20:18:13 +0800

    复习笔记

--- cs_learn/Program.cs
+++ cs_learn/Program.cs
@@ -4,8 +4,18 @@
     {
         static void Main(string[] args)
         {
-            var value = 1.2224231232;
-            Console.WriteLine($"{value, 4:F}");
+            var a = new int[][] { new int[] {1, 2, 3}, new int[] {2,3}, new int[] {3,4,5,6} };
+            var b = new int[3, 3, 3];
+            Console.WriteLine(a.Length);
+            Console.WriteLine(b.Rank);
+        }
+
+        // [Flags]
+        enum Color:uint
+        {
+            Yellow  = 0x01, //0
+            Red     = 0x02, //1
+            Blue    = 0x04  //2
         }
     }
 }
