373ddd9e82e587e4c61850f432f0098d44fb110d
wangzhiyu@maxwell-gp.com.cn <WangZhiyu>
2023-07-28 08:58:47 +0800

    23/07/28

---
+++ WPF_learn4_Property/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_learn4_Property
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Student stu;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            stu = new Student();
+            var textBox1ToStuBinding = new Binding("Text") {Source = textBox1, Mode = BindingMode.TwoWay};
+            var stuToTextBox2Binding = new Binding("Name") {Source = stu, Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged};
+            stu.SetBinding(Student.PropertyName, textBox1ToStuBinding);
+            textBox2.SetBinding(TextBox.TextProperty, stuToTextBox2Binding);
+        }
+    }
+
+    class Student : DependencyObject
+    {
+
+        public string Name
+        {
+            get => (string)GetValue(PropertyName);
+            set => SetValue(PropertyName, value);
+        }
+
+        public static readonly DependencyProperty PropertyName =
+            DependencyProperty.Register(nameof(Name), typeof(string), typeof(Student), new PropertyMetadata("学生姓名"));
+
+        public BindingExpressionBase SetBinding(DependencyProperty dp, BindingBase binding)
+        {
+            return BindingOperations.SetBinding(this, dp, binding);
+        }
+    }
+}
--- cs_learn/Program.cs
+++ cs_learn/Program.cs
@@ -6,30 +6,43 @@ namespace cs_learn
     {
         static void Main(string[] args)
         {
-            ObservableCollection<Student> students = new ObservableCollection<Student>();
-            students.Add(new Student() {Name="A", Id=1});
-            students.Add(new Student() { Name="B", Id=2});
-            students.Add(new Student() { Id=3, Name="C"});
-            students.Add(new Student() {Id=4, Name="D"});
-            students.Add(new Student() {Id=5, Name="E"});
-
-            var stu = students.ToList().Find(t => t.Id == 4);
-            if (stu  != null)
-            {
-                stu.Name = "A";
-            }
-
-            foreach (var student in students)
-            {
-                Console.WriteLine($"No.{student.Id}-{student.Name}");
-            }
-
+            // ObservableCollection<Student> students = new ObservableCollection<Student>();
+            // students.Add(new Student() {Name="A", Id=1});
+            // students.Add(new Student() { Name="B", Id=2});
+            // students.Add(new Student() { Id=3, Name="C"});
+            // students.Add(new Student() {Id=4, Name="D"});
+            // students.Add(new Student() {Id=5, Name="E"});
+            //
+            // var stu = students.ToList().Find(t => t.Id == 4);
+            // if (stu  != null)
+            // {
+            //     stu.Name = "A";
+            // }
+            //
+            // foreach (var student in students)
+            // {
+            //     Console.WriteLine($"No.{student.Id}-{student.Name}");
+            // }
+            var a = Test.A;
+            var b = Test.B;
+            Console.WriteLine(a.ToString());
+            Console.WriteLine((a | b).ToString());
+            Console.WriteLine((b | a).HasFlag(Test.A & Test.B & Test.C));
         }
 
         class Student
         {
             public string? Name { get; set; }
             public int Id {get; set; }
+        }
+
+        [Flags]
+        enum Test
+        {
+            A = 0x01,
+            B = 0x02,
+            C = 0x04,
+            D = 0x08
         }
     }
 }
